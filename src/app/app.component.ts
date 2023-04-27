fd870fe73c2fccd382a15afdd10a462c386ed230
Hrushikesh61 <hrushikeshpatil61@gmail.com>
2023-04-27 17:53:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CurrencyapidataService } from './currencyapidata.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myCurrencyConverter';
+  Currjson: any=[];
+  country1 = 'INR';
+  country2 = 'INR';
+  result:string = '1';
+
+  changecountry1(a:string){
+    this.country1 = a;
+   
+  }
+  changecountry2(a:string){
+    this.country2 = a;
+   
+  }
+
+  constructor(private currency: CurrencyapidataService){}
+  convert(){
+    //console.log(this.country1);
+    //console.log(this.country2);
+    this.currency.getcurrencydata(this.country1).subscribe(data =>
+      {
+        this.Currjson = JSON.stringify(data);
+        this.Currjson = JSON.parse(this.Currjson);
+        console.log(this.Currjson);
+        this.result = this.Currjson.rates[this.country2];
+        //console.log(this.result);
+      })
+  }
 }
